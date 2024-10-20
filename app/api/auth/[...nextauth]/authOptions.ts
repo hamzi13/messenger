4018de5ec0f3919f4312bde1a1b847c99e76206e
hamzi13 <thehamzagames0@gmail.com>
2024-10-20 10:34:45 -0700

    uu

--- app/api/auth/[...nextauth]/authOptions.ts
+++ app/api/auth/[...nextauth]/authOptions.ts
@@ -7,7 +7,7 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 import prisma from "@/app/libs/prismadb";
 
-export const authOptions: AuthOptions = {
+ const authOptions: AuthOptions = {
     adapter: PrismaAdapter(prisma),
     providers: [
       GithubProvider({
@@ -58,3 +58,5 @@ export const authOptions: AuthOptions = {
     },
     secret: process.env.NEXTAUTH_SECRET,
   };
+
+  export default authOptions
